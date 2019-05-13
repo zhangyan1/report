aaf9730c3ad2604874f8794c1c05865864d1227f
zhangyan <578033593@qq.com>
2019-05-13 15:13:11 +0800

    fix

--- report-dal/src/main/resources/sql/report_conf.sql
+++ report-dal/src/main/resources/sql/report_conf.sql
@@ -6,8 +6,8 @@ CREATE TABLE `report_conf` (
   `status` int(1)  NOT NULL COMMENT '删除状态 1-正常 0-删除',
   `type` int(1) not null comment '1-完整sql执行 2-配置表名选择字段',
   `sql_text` text not null comment 'sql文案',
-  `max_count` text not null comment '查询条数',
-  `mete_colums` text not null comment '字段转换设置',
+  `max_count` BIGINT not null DEFAULT '200' comment '查询条数',
   `source_id` bigint not null comment '数据库源id',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  key `uk_report_conf` (`source_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库配置表';
