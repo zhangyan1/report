15da41ea4ef30a518fc54319e6495d25b33e5ffb
zhangyan <578033593@qq.com>
2019-05-13 11:47:57 +0800

    fix

---
+++ report-dal/src/main/resources/sql/column_conf.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `column_conf`;
+CREATE TABLE `column_conf` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `gmt_create`   datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `status` int(1)  NOT NULL COMMENT '状态 1-正常 0-删除',
+  `report_id` bigint not null coment '报表主键id',
+  `column_name` varchar(64) name '列名',
+  `column_show_name` DEFAULT null varchar(128) name '展示列名',
+  `column_type` int(1) not null comment '1-布局列 2-统计列 3-维度列',
+  `weight` int(4) not null comment '权重',
+  `sort_type` int(1) not null DEFAULT 0 comment '排序类型',
+  `data_type` int(1) not null DEFAULT 0 comment '数据类型 0-文本 1-数字 2-json对象',
+  `json_column` text DEFAULT null comment 'json对象里面相关字段',
+  PRIMARY KEY (`id`),
+  key (`report_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库配置表';
--- report-dal/src/main/resources/sql/query_conf.sql
+++ report-dal/src/main/resources/sql/query_conf.sql
@@ -5,12 +5,13 @@ CREATE TABLE `report_conf` (
   `gmt_modified` datetime DEFAULT NULL,
   `status` int(1)  NOT NULL COMMENT '状态 1-正常 0-删除',
   `type` int(1) not null comment '1-下拉框 2-文本框 3-日期框',
-  `source_type` int(1) not null comment '1-手动配置 2-sql动态获取',
-  `query_name` varchar(32) not null comment '查询参数',
+  `source_type` int(1) not null comment '1-手动配置 2-sql动态获取(下拉框时生效)',
+  `query_name` varchar(32) not null comment '查询参数名称',
   `default_key_value`  varchar(128) DEFAULT null comment '默认展示的key(下拉框时有用)',
   `default_real_value` varchar(128) DEFAULT null comment '默认值',
   `contend` text default null comment '1-下拉框为(3|滴滴,18|同程),select ** as ** from tableName',
   `report_id` bigint not null comment '报表配置主键id',
+  `data_source_id` bigint DEFAULT  null comment '动态获取时数据源id',
   `flag` bigint not null DEFAULT 0 comment '标位 1-是否多选',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库配置表';
