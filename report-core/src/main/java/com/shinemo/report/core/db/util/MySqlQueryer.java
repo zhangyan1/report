439fe7126c31b1f997f09c1b38b971a493bf80d2
zhangyan <578033593@qq.com>
2019-05-15 16:28:40 +0800

    fix

---
+++ report-core/src/main/java/com/shinemo/report/core/db/util/MySqlQueryer.java
@@ -0,0 +1,29 @@
+package com.shinemo.report.core.db.util;
+
+
+import com.shinemo.report.client.db.domain.ReportParameter;
+import org.apache.commons.lang3.StringUtils;
+import javax.sql.DataSource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author tomdeng
+ */
+public class MySqlQueryer extends AbstractQueryer{
+
+    public MySqlQueryer(DataSource dataSource, ReportParameter parameter) {
+        super(dataSource, parameter);
+    }
+
+    @Override
+    protected String preprocessSqlText(String sqlText) {
+        sqlText = StringUtils.stripEnd(sqlText.trim(), ";");
+        final Pattern pattern = Pattern.compile("limit.*?$", Pattern.CASE_INSENSITIVE);
+        final Matcher matcher = pattern.matcher(sqlText);
+        if (matcher.find()) {
+            sqlText = matcher.replaceFirst("");
+        }
+        return sqlText + " limit 1";
+    }
+}
