a22c0b9a93133192f01679b38552c0e860f93108
zhangyan <578033593@qq.com>
2019-05-16 14:57:05 +0800

    fix

--- report-dal/src/main/resources/sql/meta_column_conf.sql
+++ report-dal/src/main/resources/sql/meta_column_conf.sql
@@ -7,13 +7,14 @@ CREATE TABLE `meta_column_conf` (
   `main_column_name` varchar(64) comment '列名',
   `column_name` varchar(64) comment '列名',
   `column_show_name` varchar(128) DEFAULT null COMMENT '展示列名',
-  `column_type` int(1) not null comment '1-布局列 2-统计列 3-维度列',
+  `column_type` int(1) not null comment '1-普通列 2-计算子列 3-计算列',
   `weight` int(4) not null comment '权重',
   `sort_type` int(1) not null DEFAULT 0 comment '排序类型 1-升序 2-降序',
   `data_type` int(1) not null DEFAULT 0 comment '数据类型 0-文本 1-数字 2-json对象',
   `extend` text DEFAULT null comment '扩展字段',
   `name` text DEFAULT null comment '模板名称',
   `orgin_data_type` varchar(16) not null comment '原始数据库字段类型',
+  `expression` varchar(1024) DEFAULT 'null' comment '由多个字段合并的列 （支持 + - * / ||）';
   PRIMARY KEY (`id`),
   key `uk_report_column` (`report_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='报表模板字段映射配置表';
