84c62b5e28bf3298a20e64a77be7a564e498caf7
zhangyan <578033593@qq.com>
2019-05-20 12:13:47 +0800

    fix

--- report-dal/src/main/resources/sql/meta_db_conf.sql
+++ report-dal/src/main/resources/sql/meta_db_conf.sql
@@ -7,11 +7,11 @@ CREATE TABLE `meta_db_conf` (
   `create_user_name` varchar(64) not null comment '创建者姓名',
   `app_type`  int(11) NOT NULL,
   `status` int  NOT NULL COMMENT '删除状态 1-正常 0-删除',
-  `user` varchar(128) not null comment '账号',
-  `passwd` varchar(256) not null comment '密码',
   `db_name` varchar(128) not null comment '数据库名称',
+ /* `user` varchar(128) not null comment '账号',
+  `passwd` varchar(256) not null comment '密码',
   `jdbc_url` varchar(256) not null comment '数据库url',
   `jdbc_driver` varchar(64) not null comment '数据库驱动',
-  `type` int(4) not null comment '1-mysql',
+  `type` int(4) not null comment '1-mysql',*/
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库配置表';
