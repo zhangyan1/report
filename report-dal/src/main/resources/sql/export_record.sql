b679413432f94fe62ba22c054b6ff0c8633d60f7
zhangyan <578033593@qq.com>
2019-05-21 10:29:37 +0800

    fix

--- report-dal/src/main/resources/sql/export_record.sql
+++ report-dal/src/main/resources/sql/export_record.sql
@@ -7,6 +7,6 @@ CREATE TABLE `export_record` (
   `create_user_name` varchar(64) not null comment '创建者姓名',
   `status` int  NOT NULL COMMENT '-1 删除 0-待审核 3-已审核 6-已完成',
   `template_id` bigint not null comment '模板id',
-  `query_params` extend default null comment '查询参数',
+  `query_params`text  default null comment '查询参数',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库导出记录表';
